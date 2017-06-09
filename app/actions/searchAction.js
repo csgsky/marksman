e5d1926bb8f55568dbc19e50aaddd3594bd70182
csgsky <674143140@qq.com>
2017-06-09 13:49:49 +0800

    search 60%

--- app/actions/searchAction.js
+++ app/actions/searchAction.js
@@ -2,9 +2,54 @@
 
 export const SEARCH_PAGE_INIT = 'SEARCH_PAGE_INIT'
 export const SEARCH_INPUT_CHANGE = 'SEARCH_INPUT_CHANGE'
-
+export const SEARCH_PAGE_BACK = 'SEARCH_PAGE_BACK'
+export const SEARCH_PAGE_CLEAR_INPUT = 'SEARCH_PAGE_CLEAR_INPUT'
+export const SEARCH_PAGE_SEARCH_INIT = 'SEARCH_PAGE_SEARCH_INIT'
+export const SEARCH_PAGE_SEARCH_DATA = 'SEARCH_PAGE_SEARCH_DATA'
+export const SEARCH_RESULT_EMPTY = 'SEARCH_RESULT_EMPTY'
 export function searchPageInit () {
   return {
     type: SEARCH_PAGE_INIT
+  }
+}
+
+export function searchPageBack () {
+  return {
+    type: SEARCH_PAGE_BACK
+  }
+}
+
+export function searchTextChange (text) {
+  return {
+    type: SEARCH_INPUT_CHANGE,
+    searchText: text
+  }
+}
+
+export function clearInput () {
+  return {
+    type: SEARCH_PAGE_CLEAR_INPUT
+  }
+}
+
+export function searchDiary (kw) {
+  return {
+    type: SEARCH_PAGE_SEARCH_INIT,
+    kw: kw
+  }
+}
+
+export function searchEmpty () {
+  console.log('searchEmpty ====> ')
+  return {
+    type: SEARCH_RESULT_EMPTY
+  }
+}
+
+export function searchDiaryData (diarys) {
+  console.warn('searchDiaryData ===> ' + diarys.length)
+  return {
+    type: SEARCH_PAGE_SEARCH_DATA,
+    diarys: diarys
   }
 }
--- app/page/SearchPage.js
+++ app/page/SearchPage.js
@@ -1,73 +1,79 @@
 import React, {Component} from 'React'
 import {StyleSheet, View, Text} from 'react-native'
 import theme from '../config/theme'
-// import * as actions from '../../actions/loginActions'
-// import { bindActionCreators } from 'redux'
-// import { connect } from 'react-redux'
+import * as actions from '../actions/searchAction'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import Swiper from 'react-native-swiper';
 import SearchTextInput from '../widget/SearchInput'
-export default class SearchPage extends Component {
-
-
-  render () {
-    const {searchText} = this.props
-    return <SearchTextInput
-      searchTextChange = {this._onSearchTextChange}
-      backPress = {this._backPress}
-      searchText={searchText}
-      title={'请输入您想要搜索的内容'}
-     />
+import EmptyView from '../component/EmptyView'
+class SearchPage extends Component {
+  componentDidMount () {
+    this.props.actions.searchPageInit()
   }
 
-
-
-  _onSearchInputClearShow = () => {
+  render () {
+    const {searchText, searchInputClearShow, empty} = this.props
+    console.warn('render ==> empty ==>  ' + empty)
+    return <View style={{flex: 1}}>
+      <SearchTextInput
+        searchTextChange = {this._onSearchTextChange}
+        backPress = {this._backPress}
+        searchText={searchText}
+        onSubmitEditing={this._onSubmitEditing}
+        searchInputClearShow={searchInputClearShow}
+        clearInput = {this._clearInput}
+        title={'请输入您想要搜索的内容'}
+      />
+      {empty && <EmptyView message={'你还未写过相关的内容哦~'}/>}
+    </View>
+    
+    
     
   }
 
   _backPress = () => {
+    this.props.actions.searchPageBack()
     this.props.navigation.goBack()
   }
 
   _onSearchTextChange = (text) => {
-    console.warn(text)
+    this.props.actions.searchTextChange(text)
   }
 
-  _onPress = () => {
-    this.props.navigation.navigate('SettingPage',{message: '设置'})
+  _onSubmitEditing = () => {
+    const {searchText} = this.props
+    this.props.actions.searchDiary(searchText)
   }
 
+  _clearInput = () => {
+    this.props.actions.clearInput()
+  }
 
-
+  // _onPress = () => {
+  //   this.props.navigation.navigate('SettingPage',{message: '设置'})
+  // }
 }
+
 const styles = StyleSheet.create({
-  lable: {
-    width: theme.screenWidth,
-    height: theme.screenWidth
-  },
-   wrapper: {
-  },
-  slide1: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
+  
 })
+
+const mapStateToProps = (state) => {
+  const {search} = state
+  return {
+    isRefreshing: search.isRefreshing,
+    searchText: search.searchText,
+    searchInputClearShow: search.searchInputClearShow,
+    empty: search.empty,
+    hasMore: search.hasMore,
+    diarys: search.diarys
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actions, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchPage)
+
