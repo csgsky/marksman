89648b0d269656c319da7b6d88e6cdac9d058859
csgsky <15043201501@163.com>
2017-06-10 15:52:44 +0800

    profile ui

---
+++ app/component/item/ProfileItem.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native'
+import theme from '../../config/theme'
+import * as consts from '../../utils/const'
+
+export default class ProfileItem extends Component {
+  render () {
+    const {value} = this.props
+    console.warn('ProfileItem ==> value ==> ' + value)
+    return <TouchableOpacity>
+      <View style={styles.item}>
+        <Image style={styles.icon} source={this._getSource(value)} ></Image>
+        <Text style={styles.text}>{value}</Text>
+      </View>
+    </TouchableOpacity>
+  }
+
+  _getSource = (type) => {
+    switch(type) {
+      case consts.PROFILE_MINE_MESSAGE:
+        return require('../../img/conment_vote.png')
+      case consts.PROFILE_MINE_FOLLOW:
+        return require('../../img/comment_share.png')
+      case consts.PROFILE_MINE_TRASH:
+        return require('../../img/conment_vote.png')
+      case consts.PROFILE_RECOMMOND_F:
+        return require('../../img/comment_share.png')
+      case consts.PROFILE_NOTIFICATION:
+        return require('../../img/conment_vote.png')
+      case consts.PROFILE_FEEDBACK:
+        return require('../../img/comment_share.png')
+      case consts.PROFILE_ABOUT_US:
+        return require('../../img/conment_vote.png')
+      default:
+        return require('../../img/comment_share.png')
+    }
+    
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 50
+  },
+  icon: {
+    width: 18,
+    height: 18
+  },
+  text: {
+    fontSize: 18,
+    marginLeft: 18,
+    color: theme.text.globalTextColor
+  }
+})
+
--- app/utils/const.js
+++ app/utils/const.js
@@ -1,1 +1,8 @@
 export const appName = '浅言'
+export const PROFILE_MINE_MESSAGE = '我的消息'
+export const PROFILE_MINE_FOLLOW = '我的关注'
+export const PROFILE_MINE_TRASH = '我的垃圾箱'
+export const PROFILE_RECOMMOND_F = '推荐给好友'
+export const PROFILE_NOTIFICATION = '系统通知'
+export const PROFILE_FEEDBACK = '反馈问题'
+export const PROFILE_ABOUT_US = '关于我们'
