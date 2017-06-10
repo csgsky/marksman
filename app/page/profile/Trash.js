8ed73664aba4677af43b0c0a8aadd1ad5bc937f9
LiuTing0403 <liuting0403@gmail.com>
2017-06-10 15:01:52 +0800

    update trash

--- app/page/profile/Trash.js
+++ app/page/profile/Trash.js
@@ -1,21 +1,51 @@
 import React, {Component} from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {View, FlatList, RefreshControl} from 'react-native'
+import {connect} from 'react-redux'
+import * as actions from '../../actions/trashActions'
+import {bindActionCreators} from 'redux'
+import DiaryItem from '../../component/item/DiaryItem'
+import ListSeparator from '../../component/ListSeperator'
 // import theme from '../../config/theme'
 
 class Trash extends Component {
+  componentDidMount () {
+    this.props.actions.trashInit()
+  }
+
   render () {
+    const {diaries, isRefreshing} = this.props
     return (
-      <View>
-        <Text style={styles.title}>I am the Trash bin</Text>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <FlatList
+          data={diaries}
+          renderItem={this.getItemCompt}
+          removeClippedSubviews={false}
+          ItemSeparatorComponent={() => <ListSeparator/>}
+          refreshControl={
+            <RefreshControl
+              onRefresh={this.onRefresh}
+              color="#ccc"
+              refreshing={isRefreshing}
+            />
+          }
+        />
       </View>
     )
   }
+  getItemCompt = ({item}) => {
+    return <DiaryItem item={item} hasComment={false}/>
+  }
 }
 
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 30
+const mapStateToProps = ({trash}) => {
+  return {
+    isRefreshing: trash.isRefreshing,
+    diaries: trash.diaries
   }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actions, dispatch)
 })
 
-export default Trash
+export default connect(mapStateToProps, mapDispatchToProps)(Trash)
