2a5c7fa8617eb987c23ea9215fda1b46010f9be3
csgsky <674143140@qq.com>
2017-06-11 13:07:25 +0800

    profile ui and router

--- app/component/item/ProfileItem.js
+++ app/component/item/ProfileItem.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react'
-import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native'
+import {View, Text, Image, TouchableOpacity, AsyncStorage, StyleSheet} from 'react-native'
 import theme from '../../config/theme'
 import * as consts from '../../utils/const'
-
+import Rx from 'rxjs'
 export default class ProfileItem extends Component {
   render () {
-    const {value} = this.props
-    console.warn('ProfileItem ==> value ==> ' + value)
-    return <TouchableOpacity>
+    const {value, navigation} = this.props
+    return <TouchableOpacity onPress={this._router}>
       <View style={styles.item}>
         <Image style={styles.icon} source={this._getSource(value)} ></Image>
         <Text style={styles.text}>{value}</Text>
@@ -36,7 +35,67 @@ export default class ProfileItem extends Component {
     }
   }
 
+  _router =  () => {
+    const that = this
+    const {value, navigation} = this.props
+    switch(value) {
+      case consts.PROFILE_MINE_MESSAGE:
+        that._routerMineMessage()
+        break;
+      case consts.PROFILE_MINE_FOLLOW:
+        that._routerMineFollow()
+        break;
+      case consts.PROFILE_MINE_TRASH:
+        that._routerMineTrash()
+        break;
+      case consts.PROFILE_RECOMMOND_F:
+        alert(consts.PROFILE_RECOMMOND_F)
+        break;
+      case consts.PROFILE_NOTIFICATION:
+        alert(consts.PROFILE_NOTIFICATION)
+        break;
+      case consts.PROFILE_FEEDBACK:
+        alert(consts.PROFILE_FEEDBACK)
+        break;
+      case consts.PROFILE_ABOUT_US:
+        alert(consts.PROFILE_ABOUT_US)
+        break;
+    }
+  }
+
+  _routerMineMessage =  () => {
+    AsyncStorage.getItem('userId').then((result) => {
+      if (result === null) {
+        this.props.navigation.navigate('Login', {come4: 'profile'})
+      } else {
+        alert('my message')
+      }
+    })
+  }
+
+  _routerMineFollow = () => {
+    AsyncStorage.getItem('userId').then((result) => {
+      if (result === null) {
+        this.props.navigation.navigate('Login', {come4: 'profile'})
+        this.props.navigation.navigate('TrashPage', {come4: 'profile'})
+      } else {
+        this.props.navigation.navigate('TrashPage', {come4: 'profile'})
+      }
+    })
+  }
+
+  _routerMineTrash = () => {
+    AsyncStorage.getItem('userId').then((result) => {
+      if (result === null) {
+        this.props.navigation.navigate('Login', {come4: 'profile'})   
+      } else {
+        this.props.navigation.navigate('TrashPage', {come4: 'profile'})
+      }
+    })
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   item: {
