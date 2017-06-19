d7b386e4b490e4172adf389edd5e260aea26c030
csgsky <15043201501@163.com>
2017-06-19 17:13:00 +0800

    finsh splash

--- app/actions/hotDiaryAction.js
+++ app/actions/hotDiaryAction.js
@@ -1,20 +1,40 @@
 export const HOTDIARY_INIT = 'HOTDIARY_INIT'
 export const HOTDIARY_DATA = 'HOTDIARY_DATA'
+export const HOTDIARY_LOADING_MORE = 'HOTDIARY_LOADING_MORE'
+export const HOTDIARY_LOADING_MORE_DATA = 'HOTDIARY_LOADING_MORE_DATA'
 
-
-export function recentDiaryInit () {
+export function hotDiaryInit (page) {
   return {
     type: HOTDIARY_INIT,
-    isRefreshing: true
+    isRefreshing: true,
+    page
   }
 }
 
-export function recentDiaryData (data) {
+export function hotDiaryData (data) {
   console.log('action  --->  HOTDIARY_DATA')
   console.log('action  --->  HOTDIARY_DATA diary length ' + data.diarys.length)
   return {
     type: HOTDIARY_DATA,
     isRefreshing: false,
-    diarys: data.diarys
+    diarys: data.diarys,
+    hasMoreData: data.diarys.length >= 6
+  }
+}
+
+export function hotDiaryLoadingMore (page) {
+  return {
+    type: HOTDIARY_LOADING_MORE,
+    isLoadingMore: true,
+    page
+  }
+}
+
+export function hotDiaryLoadingMoreData (data) {
+  return {
+    type: HOTDIARY_LOADING_MORE_DATA,
+    isLoadingMore: false,
+    diarys: data.diarys,
+    hasMoreData: data.diarys.length >= 6
   }
 }
--- app/page/WriteDiaryPage.js
+++ app/page/WriteDiaryPage.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'React'
+import React, {Component} from 'react'
 import {StyleSheet, View, Text} from 'react-native'
-import theme from '../config/theme'
+
+// import theme from '../config/theme'
+import LoadingMore from '../component/LoadingMore'
 // import * as actions from '../../actions/loginActions'
 // import { bindActionCreators } from 'redux'
 // import { connect } from 'react-redux'
@@ -8,17 +10,12 @@ import theme from '../config/theme'
 export default class WriteDiaryPage extends Component {
   render () {
     return (
-      <View>
-        <View style={styles.lable}>
-          <Text style={{color: 'red'}}>写日记</Text>
-        </View>
+      <View style={{flex: 1}}>
+        <LoadingMore />
       </View>
     )
   }
 }
 const styles = StyleSheet.create({
-  lable: {
-    width: theme.screenWidth,
-    height: theme.screenWidth
-  }
+
 })
