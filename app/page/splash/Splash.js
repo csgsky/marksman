5dbfb47f7e3f8412a4a91cebeb1a2d6ce5a4e505
csgsky <674143140@qq.com>
2017-06-19 18:30:52 +0800

    fix

--- app/page/splash/Splash.js
+++ app/page/splash/Splash.js
@@ -109,7 +109,7 @@ export default class Splash extends Component {
 
 
   _generateAuth = () => {
-    var rawStr = '/ZTE/ZTE1.1/' + this.state.devicedid + '12122/null/10.0.10.243/17695/02:00:00:00:00:00/com.droi.qy/720/1280/null'
+    var rawStr = '/ZTE/ZTE1.1/' + this.state.devicedid + '1216522/null/10.0.10.243/17695/02:00:00:00:00:00/com.droi.qy/720/1280/null'
     var words = encodeURIComponent(rawStr)
     var base64 = require('base-64').encode(words)
     const authorization = 'param=' + rawStr + '/' + CryptoJS.HmacSHA1(base64, 'qy_0_23').toString(CryptoJS.enc.Hex)
@@ -131,7 +131,7 @@ export default class Splash extends Component {
   }
 
   _getUnLoginUserInfo = (authorization) => {
-    Rx.Observable.from(getUnloginInfo(this.state.devicedid + '12122', authorization)).subscribe(
+    Rx.Observable.from(getUnloginInfo(this.state.devicedid + '1216522', authorization)).subscribe(
                       (it) => {
                         this.setState({customer: it.customer})
                         if (it.customer) {
