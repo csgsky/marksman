16c5c5954f8920514e3fec46d467afbf953a1e33
LiuTing0403 <liuting0403@foxmail.com>
2017-08-04 16:03:27 +0800

    add track and fix bug

---
+++ ios/TCAgent.h
@@ -0,0 +1,13 @@
+//
+//  Track.h
+//  marksman
+//
+//  Created by liu ting on 2017/8/4.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+
+@interface Track : NSObject <RCTBridgeModule>
+@end
