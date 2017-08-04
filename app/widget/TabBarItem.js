795ef23554a1cfeae459b1c8f80ff607b9ce4df2
LiuTing0403 <liuting0403@gmail.com>
2017-08-04 18:35:35 +0800

    try tab press

--- app/widget/TabBarItem.js
+++ app/widget/TabBarItem.js
@@ -1,11 +1,21 @@
 
-import React, { Component } from 'react'
-import { Image } from 'react-native'
+import React, { PureComponent } from 'react'
+import { Image, NativeModules } from 'react-native'
 
 // create a component
-class TabBarItem extends Component {
+class TabBarItem extends PureComponent {
+  // componentWillReceiveProps(nextProps) {
+  //   const oldFocused = this.props.focused;
+  //   const {focused, name, label} = nextProps;
+  //   console.log({focused, oldFocused})
+  //   if (focused && focused !== oldFocused) {
+  //     console.log({name, label})
+  //     NativeModules.TCAgent.track(name, label)
+  //   }
+  // }
   render () {
     let selectedImage = this.props.selectedImage ? this.props.selectedImage : this.props.normalImage
+    // console.log('render', this.props.focused)
     return (
       <Image
         resizeMode ='contain'
