a9e24dd79c0cc217d38341115a4ebaad8ce8449c
csgsky <674143140@qq.com>
2017-09-11 18:22:25 +0800

    更换引导页的图

--- app/widget/TrashModal.js
+++ app/widget/TrashModal.js
@@ -19,14 +19,14 @@ export default class PhotoPickerModal extends Component {
         transparent>
         <TouchableOpacity activeOpacity={1} style={styles.bg} onPress={this.props.hide}>
           <TouchableOpacity style={styles.dialog}>
-            <TouchableOpacity style={styles.recover} onPress={this.props.recoverDiary}>
+            <TouchableOpacity activeOpacity={0.9} style={styles.recover} onPress={this.props.recoverDiary}>
               <Text style={{fontSize: theme.text.xxlgFontSize, color: 'red'}}>恢复</Text>
             </TouchableOpacity>
             <View style={{height: 0.5, backgroundColor: '#f8f8f8'}} />
-            <TouchableOpacity style={styles.delete} onPress={this.props.deleteDiary}>
+            <TouchableOpacity activeOpacity={0.9} style={styles.delete} onPress={this.props.deleteDiary}>
               <Text style={{fontSize: theme.text.xxlgFontSize}}>彻底删除</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.cancelItem} onPress={this.props.hide}>
+            <TouchableOpacity activeOpacity={0.9} style={styles.cancelItem} onPress={this.props.hide}>
               <Text style={{fontSize: theme.text.xxlgFontSize}}>取消</Text>
             </TouchableOpacity>
           </TouchableOpacity>
